caa98b75bae55dc8527b039f85c553dfb6973d10
Kyzm7 <kyzm7@mail.ru>
2024-08-18 07:54:23 +0300

    Add files via upload

---
+++ lesson_1_5.py
@@ -0,0 +1,11 @@
+immutable_var = 2, 1, 'home', True, 'String'
+print(immutable_var)
+
+# immutable_var[1]=3  неизменяемая упорядоченная коллекция, которая может содержать
+# в себе разные типы данных.
+print(immutable_var)
+
+mutable_list=[5, 'Modified','a'], 7, 2
+mutable_list[0][0] = 10
+mutable_list[0][2] = 'b'
+print(mutable_list)
